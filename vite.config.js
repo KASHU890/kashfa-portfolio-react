976d478311ad0e27777a25ed671602d12fbd06c8
Kashfa Ahsaan <kashfa.ahsaan@gmail.com>
2026-09-22 18:36:49 +0500

    Set Pages base path for kashfa-portfolio-react

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kashfa-portfolio/',
+  base: '/kashfa-portfolio-react/',
 })
